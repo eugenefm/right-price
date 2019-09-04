4aad66a818eeed2463ce0c61dedd80e5a00b7261
Eugene Michasiw <eugene@michasiw.com>
2019-09-04 16:40:35 -0400

    Working POST api/users route

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const port = process.env.PORT;
 
 const app = express();
 
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+
 app.listen(port, () => {
   console.log('Sever is up on port: ' + port);
 });
-
-app.use(express.json());
